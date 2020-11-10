4507067411f7a2dd75817341dee57a5b37ba473f
KWeselski <weselski.kamil@gmail.com>
2020-11-10 14:28:22 +0100

    Send image to tesseract

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import datetime
 import numpy as np
 import io
 import os
+import base64
 
 app = Flask(__name__)
 app.secret_key = '\xaai\xe42\xf7\xdfEN\x02\x17\x9e\x9b\xcd-\xcf\x0cL\xc4\xbb\xeb\xa4\x10\x06'
@@ -18,67 +19,18 @@ app_dir = os.path.dirname(os.path.abspath(__file__))
 def index():
     return render_template("index.html", title="Image Converter")
 
-@app.route('/static/images/<filename>')
-def send_image(filename):
-    return send_from_directory("static/images", filename)
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    img_path = os.path.join(app_dir,'static/images/')
-    if not os.path.isdir(img_path):
-        os.mkdir(img_path)
-    upload = request.files.getlist('file')[0]
-    print('File name: {}'.format(upload.filename))
-    file = upload.filename
-
-    destination = "/".join([img_path,file])
-    upload.save(destination)
-    return render_template("processing.html",image_name=file)
-
-@app.route('/grayscale', methods=['POST'])
-def grayscale():
-    filename = request.form['image']
-    file_path = os.path.join(app_dir,'static/images/')
-    destination = "/".join([file_path,filename])
-    img = Image.open(destination)
-    if img.mode == 'RGB':
-        print('Wykonało')
-       
-    destination = "/".join([file_path, 'temp.png'])
-    #if os.path.isfile(destination):
-    #    os.remove(destination)
-    img.save(destination)
-    #send_image(filename)
-    send_image('temp.png')
-    return render_template("processing.html",image_name='temp.png')
-
 @app.route('/scan', methods=['GET', 'POST'])
 def scan():
     if request.method == 'POST':
-        image = request.files['file'].read()
-        text = pytesseract.image_to_string(Image.open(io.BytesIO(image)))  
-        image = Image.open(io.BytesIO(image)).convert('RGB')
-        #image = cv2.resize(np.array(image), None, fx=0.5, fy=0.5)
-        img_gray = get_grayscale(image)
-        adaptive_threshold = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 85, 11)
-        #img_gray = imutils.resize(img_gray, width=640) 
-        img_thresh = thresholding(img_gray)
-        img_dilate = dilate(img_thresh)
-        img_erode = erode(img_thresh)
-        #cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
-        custom_config = r'--psm 3'    
-        text_gray = pytesseract.image_to_string(img_gray, config=custom_config)
-        text_adaptive = pytesseract.image_to_string(adaptive_threshold, config=custom_config)
-        text_thresh = pytesseract.image_to_string(img_thresh, config=custom_config)
-        text_dilate = pytesseract.image_to_string(img_dilate,config=custom_config)
-        text_erode = pytesseract.image_to_string(img_erode, config=custom_config)
+        image = request.form['imageInput']
+        print(image)
+        img = Image.open(io.BytesIO(base64.b64decode(image.split(',')[1])))
+
+        text = pytesseract.image_to_string(img)
+        print(text) 
+
         session['data'] = {
             'text': text,
-            'text_adaptive': text_adaptive,
-            'text_gray': text_gray,
-            'text_thresh':text_thresh,
-            'text_dilate':text_dilate,
-            'text_erode':text_erode
         }
         return redirect(url_for('result'))
 
@@ -91,11 +43,6 @@ def result():
             'text.html',
             title= "Text scanned",
             text = data['text'],
-            text_adaptive = data['text_adaptive'],
-            text_gray= data['text_gray'],
-            text_thresh=data['text_thresh'],
-            text_dilate=data['text_dilate'],
-            text_erode=data['text_erode'],
         )
     else:
         return "Error"
--- static/processing.js
+++ static/processing.js
@@ -1,3 +1,107 @@
+console.log('script.js says "I\'m here"');
+let imgElement = document.getElementById('imageSrc');
+let inputElement = document.getElementById('fileInput');
+
+inputElement.addEventListener('change', (e) => {
+    imgElement.src = URL.createObjectURL(e.target.files[0]);
+}, false);
+
+
+let grayed = false;
+imgElement.onload = function () {
+    let mat = cv.imread(imgElement);
+    cv.imshow('canvasOutput', mat);
+    changeGray_Color();
+    callback();
+    type_blur();
+    mat.delete();
+};
+
+
+function type_blur() {
+    let blur_type = document.getElementById('blur_type')
+    let selOpt = blur_type.options[blur_type.selectedIndex].value;
+
+    let img_type = document.getElementById('image_type')
+    let imgOpt = image_type.options[image_type.selectedIndex].value;
+    let src;
+    if (imgOpt == 'source') {
+        src = cv.imread('canvasOutput');
+    }
+    else if (imgOpt == 'thresh') {
+        src = cv.imread('threshOutput');
+    }
+    if (selOpt == 'BLUR') {
+        blur(src);
+    }
+    if (selOpt == 'GAUSSIAN_BLUR') {
+        gaussian_blur(src);
+    }
+    if (selOpt == 'MEDIAN_BLUR') {
+        median_blur(src);
+    }
+    if (selOpt == 'BILATERAL_FILTER') {
+        bilateral_filter(src);
+    }
+};
+
+function blur(src) {
+    let dst = new cv.Mat();
+    let ksize = new cv.Size(3, 3);
+    let anchor = new cv.Point(-1, -1);
+    cv.blur(src, dst, ksize, anchor, cv.BORDER_DEFAULT);
+    cv.imshow('blurOutput', dst);
+    src.delete(); dst.delete();
+};
+
+function gaussian_blur(src) {
+    let dst = new cv.Mat();
+    let ksize = new cv.Size(3, 3);
+    cv.GaussianBlur(src, dst, ksize, 0, 0, cv.BORDER_DEFAULT);
+    cv.imshow('blurOutput', dst);
+    src.delete(); dst.delete();
+};
+
+function median_blur(src) {
+    let dst = new cv.Mat();
+    cv.medianBlur(src, dst, 5);
+    cv.imshow('blurOutput', dst);
+    src.delete(); dst.delete();
+};
+
+function bilateral_filter(src) {
+    let dst = new cv.Mat();
+    cv.cvtColor(src, src, cv.COLOR_RGBA2RGB, 0);
+    cv.bilateralFilter(src, dst, 9, 75, 75, cv.BORDER_DEFAULT);
+    cv.imshow('blurOutput', dst);
+    src.delete(); dst.delete();
+};
+
+function changeToGray(grayed) {
+    let button = document.getElementById('grayButton');
+    if (grayed == false) {
+        let src = cv.imread('imageSrc');
+        let dst = new cv.Mat();
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY, 0);
+        cv.imshow('canvasOutput', dst);
+        src.delete(); dst.delete();
+        button.textContent = "Change To Color";
+        return true;
+    }
+    else {
+        let src = cv.imread('imageSrc');
+        button.textContent = "Change To Gray";
+        cv.imshow('canvasOutput', src);
+        src.delete();
+        return false;
+    }
+
+};
+
+function changeGray_Color() {
+    grayed = changeToGray(grayed);
+};
+
 function thresh_binary() {
     let src = cv.imread('canvasOutput');
     let dst = new cv.Mat();
@@ -110,3 +214,12 @@ function callback() {
         thresh_adaptive_m();
     }
 };
+
+function sendImage() {
+    let image = new Image();
+    let canvas = document.getElementById('threshOutput');
+    let input = document.getElementById('imageInput');
+    image.src = canvas.toDataURL();
+    //Element.setAttribute('inp', val);
+    input.value = image.src;
+}
